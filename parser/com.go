78501e248fbe61078121a0f8c2a7e6b791c30d49
MURAOKA Taro <koron.kaoriya@gmail.com>
2017-07-20 15:59:07 +0900

    query packet

--- parser/com.go
+++ parser/com.go
@@ -1,55 +1,26 @@
 package parser
 
 import (
-	"bufio"
-	"bytes"
-	"encoding/binary"
 	"fmt"
 )
 
 type COMPacket struct {
-	FirstByte uint8
+	Raw []byte
 }
 
-func NewCOMPacket(b []byte) (*COMPacket, error) {
-	var (
-		pkt = &COMPacket{}
-	)
-	pkt.FirstByte = b[0]
-	// TODO:
-	return &COMPacket{}, nil
+type QueryPacket struct {
+	Query string
 }
 
-type OKPacket struct {
-	AffectedRows uint64
-	InsertID     uint64
-	Status       uint16
-	WarningCount uint16
-	Message      string
-}
-
-func NewOKPacket(b []byte) (*OKPacket, error) {
-	var (
-		err error
-		pkt = &OKPacket{}
-		r   = bufio.NewReader(bytes.NewReader(b[1:]))
-	)
-	if b[0] != 0x00 {
-		return nil, fmt.Errorf("OK packet must start with 0x00: %02x", b[0])
+func NewCOMPacket(b []byte) (interface{}, error) {
+	if len(b) == 0 {
+		return nil, fmt.Errorf("too short COM packet")
 	}
-	pkt.AffectedRows, err = readLengthEncodedInteger(r)
-	if err != nil {
-		return nil, err
+	switch b[0] {
+	case 0x03:
+		return &QueryPacket{Query: string(b[1:])}, nil
+	default:
+		// TODO: implement for other commands
+		return &COMPacket{Raw: b}, nil
 	}
-	pkt.InsertID, err = readLengthEncodedInteger(r)
-	err = binary.Read(r, binary.LittleEndian, &pkt.Status)
-	if err != nil {
-		return nil, err
-	}
-	err = binary.Read(r, binary.LittleEndian, &pkt.WarningCount)
-	if err != nil {
-		return nil, err
-	}
-	// FIXME: read string.
-	return pkt, nil
 }
---
+++ parser/ok.go
@@ -0,0 +1,42 @@
+package parser
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
+	"fmt"
+)
+
+type OKPacket struct {
+	AffectedRows uint64
+	InsertID     uint64
+	Status       uint16
+	WarningCount uint16
+	Message      string
+}
+
+func NewOKPacket(b []byte) (*OKPacket, error) {
+	var (
+		err error
+		pkt = &OKPacket{}
+		r   = bufio.NewReader(bytes.NewReader(b[1:]))
+	)
+	if b[0] != 0x00 {
+		return nil, fmt.Errorf("OK packet must start with 0x00: %02x", b[0])
+	}
+	pkt.AffectedRows, err = readLengthEncodedInteger(r)
+	if err != nil {
+		return nil, err
+	}
+	pkt.InsertID, err = readLengthEncodedInteger(r)
+	err = binary.Read(r, binary.LittleEndian, &pkt.Status)
+	if err != nil {
+		return nil, err
+	}
+	err = binary.Read(r, binary.LittleEndian, &pkt.WarningCount)
+	if err != nil {
+		return nil, err
+	}
+	// FIXME: read string.
+	return pkt, nil
+}
