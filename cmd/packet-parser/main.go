3f84a4f16d9fc239283f388edd3a580b3b64d003
MURAOKA Taro <koron.kaoriya@gmail.com>
2017-07-26 00:46:10 +0900

    remove unused code

--- cmd/packet-parser/main.go
+++ cmd/packet-parser/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"io"
 	"log"
 	"os"
 
@@ -11,64 +9,6 @@ import (
 	"github.com/koron/mysql-packet-sniffer/parser"
 	"github.com/koron/mysql-packet-sniffer/tcpasm"
 )
-
-var nextStrm = 0
-
-const serverPort = 3306
-
-var independentParsers = map[string]*parser.Parser{}
-
-// start MySQL packet parser in goroutine with s.
-func created(src, dst tcpasm.Endpoint, s io.ReadCloser) error {
-	if src.Port != serverPort && dst.Port != serverPort {
-		return fmt.Errorf("both port not for MySQL: src=%s dst=%s", src, dst)
-	}
-
-	n := nextStrm
-	nextStrm++
-	log.Printf("strm#%d: src=%s dst=%s", n, src, dst)
-
-	// Create a parser.
-	var pa *parser.Parser
-	var addr string
-	if src.Port == serverPort {
-		pa = parser.NewFromServer(s)
-		addr = dst.String()
-	} else {
-		pa = parser.NewFromClient(s)
-		addr = src.String()
-	}
-
-	// Share parser state.
-	if p0, ok := independentParsers[addr]; ok {
-		pa.ShareContext(p0)
-		delete(independentParsers, addr)
-	} else {
-		independentParsers[addr] = pa
-	}
-
-	go func() {
-		defer s.Close()
-		for {
-			err := pa.Parse()
-			if err == io.EOF {
-				log.Printf("strm#%d: EOF", n)
-				if len(pa.Body) > 0 {
-					log.Printf("  %#x", pa.Body)
-				}
-				return
-			}
-			if err != nil {
-				log.Printf("strm#%d: parse failed: %s", n, err)
-				return
-			}
-			// show last parsed MySQL packet.
-			log.Printf("strm#%d: %s", n, pa.String())
-		}
-	}()
-
-	return nil
-}
 
 type conn struct {
 	addr tcpasm.Endpoint
